bd2ef16bbbe63d427ef377d495972120e66869a8
soongzx <monkeycode-ai@chaitin.com>
2026-04-24 10:37:27 +0000

    chore: test with minimal module - only EmptyConfigProvider
    Stripped down to bare minimum to isolate the toast error.
    Co-authored-by: monkeycode-ai <monkeycode-ai@chaitin.com>

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,29 @@
-import { NgModule } from '@angular/core'
+import { Injectable, NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { FormsModule } from '@angular/forms'
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
-import TabbyCoreModule, { ConfigProvider, ProfileProvider } from 'tabby-core'
-import { SettingsTabProvider } from 'tabby-settings'
-import TabbyTerminalModule from 'tabby-terminal'
+import TabbyCoreModule, { ConfigProvider } from 'tabby-core'
 
-import { ManagedSSHConfigProvider } from './config'
-import { ManagedSSHProfilesService } from './profiles'
-import { ManagedSSHSettingsTabProvider } from './settings'
-import { ManagedSSHSettingsTabComponent } from './components/managedSSHSettingsTab.component'
-import { ManagedSSHStoreService } from './services/managedSSHStore.service'
-import { ManagedSSHSecretService } from './services/managedSSHSecret.service'
-import { ManagedSSHMapperService } from './services/managedSSHMapper.service'
-import { ManagedSSHLauncherService } from './services/managedSSHLauncher.service'
-import { ManagedSSHUtilsService } from './services/managedSSHUtils.service'
+@Injectable()
+class EmptyConfigProvider extends ConfigProvider {
+    defaults = {
+        sshCredentialHub: {
+            version: 1,
+            profiles: [],
+            preferences: {
+                preferVault: true,
+                showManagedProfilesOnly: true,
+                defaultGroup: ''
+            }
+        }
+    }
+}
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    NgbModule,
-    TabbyCoreModule,
-    TabbyTerminalModule
-  ],
-  declarations: [
-    ManagedSSHSettingsTabComponent
-  ],
-  entryComponents: [
-    ManagedSSHSettingsTabComponent
-  ],
-  providers: [
-    { provide: ConfigProvider, useClass: ManagedSSHConfigProvider, multi: true },
-    { provide: SettingsTabProvider, useClass: ManagedSSHSettingsTabProvider, multi: true },
-    { provide: ProfileProvider, useExisting: ManagedSSHProfilesService, multi: true },
-    ManagedSSHStoreService,
-    ManagedSSHSecretService,
-    ManagedSSHMapperService,
-    ManagedSSHLauncherService,
-    ManagedSSHUtilsService
-  ]
+    imports: [
+        CommonModule,
+        TabbyCoreModule
+    ],
+    providers: [
+        { provide: ConfigProvider, useClass: EmptyConfigProvider, multi: true }
+    ]
 })
 export default class ManagedSSHModule {}
